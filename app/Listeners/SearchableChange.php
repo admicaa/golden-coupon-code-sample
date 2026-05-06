dd476da247bbeb7cc49bfaf3459198997e57a3d9
Ahmed Thabet <almestaadmica@gmail.com>
2026-05-06 15:36:36 +0300

    perf: queue searchable index updates
    - make SearchableChange listener implement ShouldQueue
    - add retry, delay, and failed handler
    - cast values to string before strip_tags

--- app/Listeners/SearchableChange.php
+++ app/Listeners/SearchableChange.php
@@ -7,8 +7,13 @@ use App\Models\Search;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SearchableChange
+class SearchableChange implements ShouldQueue
 {
+    use InteractsWithQueue;
+
+    public $delay = 5;
+    public $tries = 3;
+
     /**
      * Create the event listener.
      *
@@ -27,12 +32,18 @@ class SearchableChange
      */
     public function handle(SearchableChangeEvent $event)
     {
-        //
         $searchable = $event->searchable;
         $searchUpdate = $searchable->SearchUpdate;
+
         foreach ($searchUpdate as $key => $value) {
-            $searchUpdate[$key] = html_entity_decode(strip_tags($value));
+            $searchUpdate[$key] = html_entity_decode(strip_tags((string) $value));
         }
+
         Search::updateOrCreate($searchable->SearchColumn, $searchUpdate);
+    }
+
+    public function failed(SearchableChangeEvent $event, $exception)
+    {
+        report($exception);
     }
 }
